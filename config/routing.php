0396642a32ef0b2fce15b060036f3d86e5f195c6
mathieuphung <phung.mathieu@gmail.com>
2016-02-06 21:45:16 +0100

    Push composer.json + router correction

--- config/routing.php
+++ config/routing.php
@@ -1,4 +1,14 @@
 <?php
-$routes = [
-    '/index.php?url=Test' => 'Test'
-];
+$router = new Library\Router($_GET['url']);
+
+$router->get('/', 'Home#defaultAction');
+$router->get('/posts', function () {
+    echo 'Tous les articles';
+});
+$router->get('/posts/:slug-:id/:page', 'Posts#show')->with('id', '[0-9]+')->with('page', '[0-9]+')->with('slug', '([a-z\-0-9]+)');
+$router->get('/posts/:id', "Posts#show");
+$router->post('/posts/:id', function ($id) {
+    echo 'Poster pour l\'article ' . $id . '<pre>' . print_r($_POST, true) . '</pre>';
+});
+
+$router->run();
--- public/index.php
+++ public/index.php
@@ -19,13 +19,4 @@ $response = $router->run($route);
 */
 
 require '../vendor/autoload.php';
-
-$router = new Library\Router($_GET['url']);
-
-$router->get('/', function(){ echo 'Homepage'; });
-$router->get('/posts', function(){ echo 'Tous les articles'; });
-$router->get('/posts/:slug-:id/:page', 'Posts#show')->with('id', '[0-9]+')->with('page', '[0-9]+')->with('slug', '([a-z\-0-9]+)');
-$router->get('/posts/:id', "Posts#show");
-$router->post('/posts/:id', function($id){ echo 'Poster pour l\'article ' . $id . '<pre>' . print_r($_POST, true) . '</pre>'; });
-
-$router->run();
+require_once '../config/config.php';
